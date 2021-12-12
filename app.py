15649ad9e49bfdae47cbe54c20bb528ef37eac9f
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-12-12 22:02:33 +0600

    Change

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 # arr = [279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279]
-for x in range(23):
+for x in range(1):
     d = '279 days ago'
     with open('file.txt','a') as file:
         file.write(d)
