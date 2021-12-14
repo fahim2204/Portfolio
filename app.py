5323d47929fe915c09f260cebcfdb86c1174bd17
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-12-14 22:08:08 +0600

    Change

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 # arr = [279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279]
-for x in range(10):
-    d = '278 days ago'
+for x in range(24):
+    d = '277 days ago'
     with open('file.txt','a') as file:
         file.write(d)
     os.system('git add .')
     os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(10):
-    d = '271 days ago'
+for x in range(20):
+    d = '270 days ago'
     with open('file.txt','a') as file:
         file.write(d)
     os.system('git add .')
