bd5f699601228ba909a9166585157206df22bf08
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-12-18 22:21:18 +0600

    Change

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
 import os
 # arr = [279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279]
-for x in range(24):
-    d = '276 days ago'
+for x in range(8):
+    d = '273 days ago'
     with open('file.txt','a') as file:
         file.write(d)
     os.system('git add .')
     os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(24):
-    d = '269 days ago'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(19):
-    d = '275 days ago'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(19):
-    d = '268 days ago'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(14):
-    d = '274 days ago'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(14):
-    d = '267 days ago'
+for x in range(9):
+    d = '266 days ago'
     with open('file.txt','a') as file:
         file.write(d)
     os.system('git add .')
