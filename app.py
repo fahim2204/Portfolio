b4839696cd0c84f6caf6c85ee38a45cb9fa5e6b5
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-12-23 21:44:36 +0600

    Change

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 import os
 from random import randint
 
-d = '269 days ago'
+d = '268 days ago'
 with open('file.txt','a') as file:
     file.write(d)
 os.system('git add .')
 os.system('git commit --date="'+d+'" -m "Change"')
+d = '267 days ago'
+with open('file.txt','a') as file:
+    file.write(d)
+os.system('git add .')
+os.system('git commit --date="'+d+'" -m "Change"')
+d = '266 days ago'
+with open('file.txt','a') as file:
+    file.write(d)
+os.system('git add .')
+os.system('git commit --date="'+d+'" -m "Change"')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 os.system("git push")
