3dae8cf7ccd9fa51c8f7674fb74fcfd2839369eb
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-12-29 12:00:00 +0600

    Change

--- app.py
+++ app.py
@@ -1,40 +1,17 @@
 import os
 # arr = [279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279,279]
-for x in range(33):
-    d = '2021-12-27 12:00:00'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(4):
-    d = '2021-12-26 12:00:00'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(29):
-    d = '2022-01-02 12:00:00'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(29):
-    d = '2022-01-03 12:00:00'
+for x in range(24):
+    d = '2021-12-29 12:00:00'
     with open('file.txt','a') as file:
         file.write(d)
     os.system('git add .')
     os.system('git commit --date="'+d+'" -m "Change"')
 for x in range(19):
-    d = '2022-01-09 12:00:00'
+    d = '2022-01-05 12:00:00'
     with open('file.txt','a') as file:
         file.write(d)
     os.system('git add .')
     os.system('git commit --date="'+d+'" -m "Change"')
-for x in range(19):
-    d = '2022-01-10 12:00:00'
-    with open('file.txt','a') as file:
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+d+'" -m "Change"')
+
 
 os.system("git push")
